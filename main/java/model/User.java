d1cc58c779fa4f81d3a5ca2f1e2998e4c18f73b0
XavierCanadas <xavier.canadas01@estudiant.upf.edu>
2025-05-09 12:29:56 +0200

    Added the missing attributes to User with their getters and setters.

--- main/java/model/User.java
+++ main/java/model/User.java
@@ -1,15 +1,22 @@
 package model;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class User implements Serializable {
 
 	private static final long serialVersionUID = -8465990321138923924L;
 
-	private int id;
-	private String name;
+	// todo: Posem el role?
+	private int id; // primary key auto increment
+	private String username;
 	private String password;
 	private String email;
+	private String gender;
+	private Date birthdate;
+	private String polis;
+
+
 
 	public User() {
 		super();
@@ -23,12 +30,12 @@ public class User implements Serializable {
 		this.id = id;
 	}
 
-	public String getName() {
-		return name;
+	public String getUsername() {
+		return username;
 	}
 
-	public void setName(String name) {
-        this.name = name;
+	public void setUsername(String username) {
+        this.username = username;
 	}
 
 	public String getPassword() {
@@ -47,4 +54,27 @@ public class User implements Serializable {
 		this.email = email;
 	}
 
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Date getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public String getPolis() {
+		return polis;
+	}
+
+	public void setPolis(String polis) {
+		this.polis = polis;
+	}
 }
